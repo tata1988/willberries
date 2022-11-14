4657812ab8fc2739f3568ff872075216060eef7a
Tatyana <nikolina.tatyana@yandex.ru>
2022-11-15 01:07:35 +0500

    интенсив в Gloacademy

---
+++ js/cart.js
@@ -0,0 +1,157 @@
+const cart = () => {
+
+    const cartBtn = document.querySelector('.button-cart');
+    const cart = document.getElementById('modal-cart');
+    const closeBtn = document.querySelector('.modal-close');
+    const goodsContainer = document.querySelector('.long-goods-list');
+    const cartTable = document.querySelector('.cart-table__goods');
+    const modalForm = document.querySelector('.modal-form');
+
+    const deleteCartItem = (id) => {
+        const cart = JSON.parse(localStorage.getItem('cart'));
+
+        const newCart = cart.filter(good => {
+            return good.id !== id
+        });
+
+        localStorage.setItem('cart', JSON.stringify(newCart));
+        renderCartGoods(JSON.parse(localStorage.getItem('cart')));
+    }
+
+    const plusCartItem = (id) => {
+        const cart = JSON.parse(localStorage.getItem('cart'));
+        const newCart = cart.map(good => {
+            if (good.id === id) {
+                good.count++;
+            }
+            return good
+        })
+        localStorage.setItem('cart', JSON.stringify(newCart));
+        renderCartGoods(JSON.parse(localStorage.getItem('cart')));
+    }
+
+    const minusCartItem = (id) => {
+        const cart = JSON.parse(localStorage.getItem('cart'));
+        const newCart = cart.map(good => {
+            if (good.id === id) {
+                if (good.count > 0) {
+                    good.count--;
+                }
+            }
+            return good
+        })
+        localStorage.setItem('cart', JSON.stringify(newCart));
+        renderCartGoods(JSON.parse(localStorage.getItem('cart')));
+    }
+
+    const addToCart = (id) => {
+        const goods = JSON.parse(localStorage.getItem('goods'));
+        const clikedGood = goods.find(good => good.id === id);
+        const cart = localStorage.getItem('cart') ?
+            JSON.parse(localStorage.getItem('cart')) : [];
+
+
+        if (cart.some(good => good.id === clikedGood.id)) {
+            cart.map(good => {
+                if (good.id === clikedGood.id) {
+                    good.count++;
+                }
+                return good
+            })
+        } else {
+            console.log('добавить');
+            clikedGood.count = 1;
+            cart.push(clikedGood);
+        }
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }
+
+    const renderCartGoods = (goods) => {
+        cartTable.innerHTML = '';
+
+        goods.forEach(good => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+            <td>${good.name}</td>
+            <td>${good.price}$</td>
+            <td><button class="cart-btn-minus">-</button></td>
+            <td>${good.count}</td>
+            <td><button class="cart-btn-plus">+</button></td>
+            <td>${+good.price * +good.count}</td>
+            <td><button class="cart-btn-delete">x</button></td>
+            `
+
+            cartTable.append(tr);
+
+            tr.addEventListener('click', (e) => {
+                console.log(e.target);
+                if (e.target.classList.contains('cart-btn-minus')) {
+                    minusCartItem(good.id);
+                } else if (e.target.classList.contains('cart-btn-plus')) {
+                    plusCartItem(good.id);
+                } else if (e.target.classList.contains('cart-btn-delete')) {
+                    deleteCartItem(good.id);
+                }
+            })
+        })
+    }
+
+    const sendForm = () => {
+        const cartArray = localStorage.getItem('cart') ?
+            JSON.parse(localStorage.getItem('cart')) : [];
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                cart: cartArray,
+                name: '',
+                phone: ''
+            })
+        }).then(() => {
+            cart.style.display = '';
+        })
+    }
+
+    modalForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        sendForm();
+    })
+
+    cartBtn.addEventListener('click', () => {
+        const cartArray = localStorage.getItem('cart') ?
+            JSON.parse(localStorage.getItem('cart')) : [];
+        renderCartGoods(cartArray)
+        cart.style.display = 'flex';
+    });
+
+    closeBtn.addEventListener('click', () => {
+        cart.style.display = '';
+    })
+
+    cart.addEventListener('click', (e) => {
+        if (!e.target.closest('.modal') && e.target.classList.contains('overlay')) {
+            cart.style.display = '';
+        }
+    })
+
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            cart.style.display = '';
+        }
+    })
+
+    if (goodsContainer) {
+        goodsContainer.addEventListener('click', (e) => {
+            if (e.target.closest('.add-to-cart')) {
+                const buttonToCart = e.target.closest('.add-to-cart');
+                const goodId = buttonToCart.dataset.id;
+                addToCart(goodId);
+
+            }
+        })
+    }
+
+}
+
+cart();
---
+++ js/getGoods.js
@@ -0,0 +1,67 @@
+
+const getGoods = () => {
+    const links = document.querySelectorAll('.navigation-link');
+    const more = document.querySelector('.more');
+
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list');
+        goodsContainer.innerHTML = '';
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div');
+            goodBlock.classList.add('col-lg-3');
+            goodBlock.classList.add('col-sm-6');
+            goodBlock.innerHTML = `
+                <div class="goods-card">
+                    <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                    <img src= 'db/${good.img}' alt=${good.name} class="goods-image">
+                    <h3 class="goods-title">${good.name}</h3>
+                    <p class="goods-description">${good.description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id=${good.id}>
+                        <span class="button-price">$${good.price}</span>
+                    </button>
+				</div>
+            `
+            goodsContainer.append(goodBlock);
+            console.log(good);
+        })
+    }
+
+    const getData = (linkValue, category) => {
+        fetch('../db/db.json')
+            .then((res) => res.json())
+            .then((data) => {
+                const array = category ? data.filter((item) => item[category] === linkValue) : data;
+                localStorage.setItem('goods', JSON.stringify(array));
+
+                if (window.location.pathname !== '/goods.html') {
+                    window.location.href = 'goods.html'
+                } else {
+                    renderGoods(array);
+                }
+
+            })
+    }
+
+    links.forEach((link) => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const linkValue = link.textContent;
+            const category = link.dataset.field;
+
+            getData(linkValue, category);
+        })
+    })
+
+    if (localStorage.getItem('goods') && (window.location.pathname !== '/goods.html')) {
+        renderGoods(JSON.parse(localStorage.getItem('goods')))
+    }
+
+    if (more) {
+        more.addEventListener('click', (e) => {
+            e.preventDefault();
+            getData();
+        })
+    }
+}
+
+getGoods()
